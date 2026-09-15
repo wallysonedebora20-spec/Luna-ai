70b540cdd6119781b19f1efe3b74a26ffcca9a07
wallysonedebora20-spec <wallysonedebora20@gmail.com>
2026-09-15 17:00:31 +0100

    Add CORS support to the server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import OpenAI from "openai";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const client = new OpenAI({
